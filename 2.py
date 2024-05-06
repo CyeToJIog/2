a6ebe46e0c35ab2593ecfdaf490850f6b456cdee
CyeToJIog <168110110+CyeToJIog@users.noreply.github.com>
2024-05-06 20:05:05 +0300

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,7 @@
+phone_book = {}
+phone_book['Илья'] = "+79999999999"
+phone_book['Иван'] = "+78888888888"
+phone_book['Анна'] = "+76666666666"
+phone_book['Катя'] = "+74444444444"
+phone_book['Инна'] = "+79898989898"
+print(phone_book)
---
+++ 3.py
@@ -0,0 +1,7 @@
+var = "123456789, Denis"
+print(var)
+var1 = var.replace("8", "*")
+var2 = var1.replace("6", "^")
+var3 = var2.replace("3", "№")
+print(var3.upper())
+
